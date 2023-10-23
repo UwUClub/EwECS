33b6bcd60caeb71a0f131bbb80197c7338ad2239
Glassalo <maxence.labourel@epitech.eu>
2023-10-23 18:57:58 +0200

    Fixed compilation

--- src/Asset/AssetManager.cpp
+++ src/Asset/AssetManager.cpp
@@ -1,6 +1,6 @@
 #include "AssetManager.hpp"
 
-ECS::Asset::AssetManager &ECS::Asset::AssetManager::getInstance()
+ECS::Asset::AssetManager &ECS::Asset::AssetManager::getInstance() noexcept
 {
     static AssetManager instance;
 
