9837933fc4ce079e057fadc5ec01e028d4dcf58f
Glassalo <maxence.labourel@epitech.eu>
2023-10-31 09:08:40 +0100

    Added path for installed program

--- includes/EwECS/Utils.hpp
+++ includes/EwECS/Utils.hpp
@@ -1,6 +1,10 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <libgen.h>
+#include <linux/limits.h>
+#include <string>
+#include <unistd.h>
 namespace ECS::Utils {
     struct Vector2i
     {
@@ -21,6 +25,25 @@ namespace ECS::Utils {
             float x;
             float y;
     };
+
+    static std::string getFilePathInstall()
+    {
+#if defined(__linux__)
+        char result[PATH_MAX];
+        ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+        if (count < 0 || count >= PATH_MAX) {
+            return "./";
+        }
+        result[count] = '\0';
+        char *dir = dirname(result);
+        if (dir == nullptr) {
+            return "./";
+        }
+        return (std::string(dir) + "/");
+#else
+        return "./";
+#endif
+    }
 } // namespace ECS::Utils
 
 #endif // !
