1ca4b3bbc1265fb1e27bb5fbf772e6c179b2033b
PatatOfour <61944819+GlassAlo@users.noreply.github.com>
2023-11-01 00:30:02 +0000

    Fix abort on windows

--- src/Network/NetworkHandler.cpp
+++ src/Network/NetworkHandler.cpp
@@ -28,14 +28,18 @@ namespace ECS::Network {
 
     void NetworkHandler::listen()
     {
-        _readBuffer = _readInbound.prepare(READ_BUFFER_SIZE);
-        _socket.async_receive_from(boost::asio::buffer(_readBuffer), _readEndpoint,
-                                   boost::bind(&NetworkHandler::tryHandleRequest, this,
-                                               boost::asio::placeholders::error,
-                                               boost::asio::placeholders::bytes_transferred));
+        try {
+            _readBuffer = _readInbound.prepare(READ_BUFFER_SIZE);
+            _socket.async_receive_from(boost::asio::buffer(_readBuffer), _readEndpoint,
+                                    boost::bind(&NetworkHandler::tryHandleRequest, this,
+                                                boost::asio::placeholders::error,
+                                                boost::asio::placeholders::bytes_transferred));
 
-        if (!_ioThread.joinable()) {
-            _ioThread = boost::thread(boost::bind(&boost::asio::io_service::run, &_ioService));
+            if (!_ioThread.joinable()) {
+                _ioThread = boost::thread(boost::bind(&boost::asio::io_service::run, &_ioService));
+            }
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
         }
     }
 
@@ -77,24 +81,36 @@ namespace ECS::Network {
         } catch (const std::exception &e) {
             send(ERROR_PACKET_TYPE, _readEndpoint);
         }
+        try {
         _readInbound.consume(_readInbound.size());
         listen();
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
 
     void NetworkHandler::send(int8_t aType, const udp::endpoint &aEndpoint)
     {
+        try {
         PacketHeader header(aType);
 
         std::vector<uint8_t> strBuff = serialize(header);
         _socket.send_to(boost::asio::buffer(strBuff), aEndpoint);
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
 
     void NetworkHandler::sendAknowledgment(std::string &aUuid, const udp::endpoint &aEndpoint)
     {
+        try {
         PacketHeader header(AKNOWLEDGMENT_PACKET_TYPE, aUuid);
 
         std::vector<uint8_t> strBuff = serialize(header);
         _socket.send_to(boost::asio::buffer(strBuff), aEndpoint);
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
 
     boost::asio::io_service &NetworkHandler::getIoService()
@@ -104,6 +120,7 @@ namespace ECS::Network {
 
     void NetworkHandler::stop()
     {
+        try {
         for (auto &sender : _senders) {
             sender.second.second = false;
             if (sender.second.first.joinable()) {
@@ -115,6 +132,9 @@ namespace ECS::Network {
             _ioThread.join();
         }
         _socket.close();
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
 
 } // namespace ECS::Network
